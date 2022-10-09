cb168dce36b3631fc0df6a6d99f8e3c4bfc438e1
aimansae <saeedaimann@gmail.com>
2022-10-09 10:24:28 +0000

    add task added in task.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,13 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization check materialize doc
+document.addEventListener('DOMContentLoaded', function () {
+// sidenav initialization check materialize doc
 
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+var sidenav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sidenav);
 
 // modal
 
-document.addEventListener('DOMContentLoaded', function() {
-  var modal = document.querySelectorAll('.modal');
-  M.Modal.init(modal);
+var modal = document.querySelectorAll('.modal');
+M.Modal.init(modal);
+//date picker
+
+
+var datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+  format: "dd mmm, yyyy",
+  i18n: {
+    done: "Select"
+  }
+});
+// select initialization
+var selects = document.querySelectorAll('select');
+M.For
+// collapsible initialization
+
+var collapsibles = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsibles);
+
 });
