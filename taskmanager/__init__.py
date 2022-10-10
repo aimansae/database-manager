8385ed347822dc9ac151cebe854d359019ac5833
aimansae <saeedaimann@gmail.com>
2022-10-10 18:13:29 +0000

    Add posgresql function

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,5 +1,6 @@
 
 import os
+import re
 from flask import Flask  # capiitalF
 from flask_sqlalchemy import SQLAlchemy
 # to use the hidden environment variables also after deployment
@@ -14,7 +15,10 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    uri = environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://","postgresql://", 1 )
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 # always like this craete instance of SQLAlchemy class
 # set it to app instance
 
