7388f46c91a6dc3f0fa527795cb0f0f499fd1bc7
aimansae <saeedaimann@gmail.com>
2022-10-10 18:05:14 +0000

    Update __init__.py file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -14,7 +14,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 # always like this craete instance of SQLAlchemy class
 # set it to app instance
 
